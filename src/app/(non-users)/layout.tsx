4ac73af89cf7a82f7edb34e6ececda18192a05e2
singilwon <joowoni98@naver.com>
2025-11-11 14:23:29 +0900

    🐛 layout 함수 이름 변경

--- src/app/(non-users)/layout.tsx
+++ src/app/(non-users)/layout.tsx
@@ -1,3 +1,3 @@
-export default function nonUserLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
