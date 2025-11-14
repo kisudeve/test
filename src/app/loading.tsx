f2b9c7bbefa7fa1e362cd0e9a313e8e668aa2d2d
ivoryShield <sangapark34@naver.com>
2025-11-14 15:21:13 +0900

    ⚡️로딩, 스켈레톤 작업

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,25 @@
 export default function Loading() {
   return (
     <>
-      <h1>Loading</h1>
+      <div className="w-full h-screen gap-3 pt-40 pb-40 relative flex items-center justify-center animate-pulse">
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.1s]">
+          <div className="w-1 h-6 bg-blue-200"></div>
+          <div className="w-3 h-12 bg-blue-200 rounded-sm"></div>
+          <div className="w-1 h-6 bg-blue-200"></div>
+        </div>
+
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.2s]">
+          <div className="w-1 h-6 bg-violet-200"></div>
+          <div className="w-3 h-12 bg-violet-200 rounded-sm"></div>
+          <div className="w-1 h-6 bg-violet-200"></div>
+        </div>
+
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.1s]">
+          <div className="w-1 h-6 bg-purple-200"></div>
+          <div className="w-3 h-12 bg-purple-200 rounded-sm"></div>
+          <div className="w-1 h-6 bg-purple-200"></div>
+        </div>
+      </div>
     </>
   );
 }
