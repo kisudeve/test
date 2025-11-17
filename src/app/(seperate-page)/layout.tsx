59a587d9d1dc319297ce1c2172fa0129ddce7101
ivoryShield <sangapark34@naver.com>
2025-11-18 00:15:00 +0900

    💄헤더 반응형

--- src/app/(seperate-page)/layout.tsx
+++ src/app/(seperate-page)/layout.tsx
@@ -6,15 +6,9 @@ export default async function SeperateLayout({ children }: { children: React.Rea
   const [userProfile, todayScore] = await Promise.all([getUserProfile(), getTodayScore()]);
 
   return (
-    <div className="flex min-w-full min-h-screen">
-      <div className="py-6 pl-4 w-[18%] min-w-[180px] shrink-0 sticky top-0 h-screen overflow-y-auto">
-        <Header initialProfile={userProfile} todayScore={todayScore} />
-      </div>
-      <main className="flex-1">{children}</main>
+    <div className="flex min-w-full min-h-full">
+      <Header initialProfile={userProfile} todayScore={todayScore} />
+      <main className="flex-1 p-6 pt-25 xl:pt-6 min-h-screen">{children}</main>
     </div>
   );
 }
-
-export const metadata = {
-  title: "Public",
-};
