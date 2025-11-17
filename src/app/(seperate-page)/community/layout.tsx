1d039c2eb3de014a9116ff4657f9e4dbb1f08450
ivoryShield <sangapark34@naver.com>
2025-11-17 12:33:14 +0900

    💄레이아웃 1차 수정(폴더구조 변경)

--- src/app/(seperate-page)/community/layout.tsx
+++ src/app/(seperate-page)/community/layout.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function CommunityLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
-      <div className="py-6 px-4 flex-1 flex flex-col gap-6">{children}</div>
+      <div className="p-6 flex-1 flex flex-col gap-6">{children}</div>
       <div className="w-[21.95%] min-w-[180px] py-6 pr-4">
         <div className="flex flex-col sticky top-6 gap-4">
           <Suspense fallback={<TrendTagsSkeleton />}>
