cc124ab337669999bfe660c26e58cc81e3e0de3c
elecharu <elec_haru@naver.com>
2025-11-18 12:27:49 +0900

    ✅알림 페이지 다크모드 적용

--- src/app/(seperate-page)/profile/[userId]/loading.tsx
+++ src/app/(seperate-page)/profile/[userId]/loading.tsx
@@ -2,7 +2,7 @@ import ProfileSkeleton from "@/components/skeleton/ProfileSkeleton";
 
 export default function Loading() {
   return (
-    <div className="w-full space-y-6 dark:bg-[#141c2c]">
+    <div className="w-full space-y-6">
       <ProfileSkeleton />
     </div>
   );
